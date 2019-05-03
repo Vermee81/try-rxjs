6c0052a254bc3d3a8f42780678ab1212ed80001d
Vermee81 <hrksb5029@gmail.com>
2019-05-04 08:11:23 +0900

    [update] ofメソッドを使って書き換え
    http://reactivex.io/rxjs/class/es6/Observable.js~Observable.html#static-method-of

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
 import * as Rx from 'rxjs';
 
+var observable_1 = Rx.of(1, 2, 3);
+
 var observer_1 = {
   next: x => console.log('Observer1 got a value: ' + x),
   error: err => console.log('Observer1 got an error: ' + err),
   complete: () => console.log('Observer1 got Complete'),
 }; // データを受け取った時に何をするのかが書かれている
-
-function subscribe_1(observer){
-  observer.next(1);
-  observer.next(2);
-  observer.next(3);
-  observer.complete();
-}// 指定のObserverに、『1,2,3』というデータと『完了』を送る
-
-var observable_1 = Rx.Observable.create(subscribe_1);
 
 observable_1.subscribe(observer_1);
 // subscribe_1が送るデータの受け取り手としてobserver_1を指定する

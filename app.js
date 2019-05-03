e291ab1fb645e1a8165a840b6b8574e70366a1e3
Vermee81 <hrksb5029@gmail.com>
2019-05-04 08:54:26 +0900

    [update] 3つめに1秒のタイムラグを入れる

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 import * as Rx from 'rxjs';
 
-var observable_1 = Rx.of(1, 2, 3);
-
 var observer_1 = {
   next: x => console.log('Observer1 got a value: ' + x),
   error: err => console.log('Observer1 got an error: ' + err),
   complete: () => console.log('Observer1 got Complete'),
 }; // データを受け取った時に何をするのかが書かれている
 
-observable_1.subscribe(observer_1);
-// subscribe_1が送るデータの受け取り手としてobserver_1を指定する
+function subscribe_1(observer){
+  observer.next(1);
+  observer.next(2);
+  setTimeout(function(){
+    observer.next(3);
+  }, 1000);
+}
 
-Rx.of(1, 2, 3).subscribe(x => console.log('Observer1 got a value: ' + x), undefined, _ => console.log('Observer1 got Complete') );
+// create Observable object
+var observable_1 = Rx.Observable.create(subscribe_1);
+
+console.log("before");
+observable_1.subscribe(observer_1);
+console.log("after");
